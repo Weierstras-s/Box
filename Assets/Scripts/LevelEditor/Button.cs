349b1b47a30c0cbc040ef03527341f8c1c9b8470
Weierstras-s <55151155+Weierstras-s@users.noreply.github.com>
2021-12-20 22:09:02 +0800

    Add editor

---
+++ Assets/Scripts/LevelEditor/Button.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace LevelEditor {
+    public class Button : MonoBehaviour {
+        public Button btn;
+        public Text txt;
+        public void Save() {
+            Debug.Log("Hello");
+        }
+    }
+}
